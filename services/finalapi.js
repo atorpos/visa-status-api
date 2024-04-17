a36cd09b4aef224cc0dbbdc47378c62a63bedded
Oskar Wong <atorpos@mac.com>
2024-04-17 08:37:15 -0400

    port update

--- services/finalapi.js
+++ services/finalapi.js
@@ -4,7 +4,7 @@ const config = require("../config");
 
 async function getMultiple(page = 1) {
     const offset = helper.getOffset(page, config.listPerPage);
-    const rows = await db.query("");
+    const rows = await db.query(`Select * from visa_container order by id desc LIMIT ${offset}, ${config.listPerPage}`);
 
     const data = helper.emptyOrRows(rows);
     const meta = {page};
@@ -16,7 +16,10 @@ async function getMultiple(page = 1) {
 }
 
 async function create(finalapi) {
-    const result = await db.query(``);
+    finalapi.approvetime = undefined;
+    finalapi.casestatus = undefined;
+    finalapi.caseid = undefined;
+    const result = await db.query(`INSERT INTO eb5table (case_id, case_status, update_date, approve_date) VALUES ('${finalapi.caseid}', '${finalapi.casestatus}','${finalapi.time}','${finalapi.approvetime}')`);
 
     let message = "Error in models";
     if (result.affectedRows) {
@@ -28,7 +31,7 @@ async function create(finalapi) {
 }
 
 async function update(id, finalapi) {
-    const result = await db.query(``);
+    const result = await db.query(`UPDATE eb5update SET case_status="${finalapi.casestatus}", create_time=${finalapi.time} WHERE id=${id}`);
     let message = "Error in models";
     if (result.affectedRows) {
         message = "record updated";
@@ -39,7 +42,7 @@ async function update(id, finalapi) {
 }
 
 async function remove(id) {
-    const result = await db.query(`DELETE FROM table_name WHERE id = ?`, [id]);
+    const result = await db.query(`DELETE FROM eb5update WHERE id = ${id}`);
     let message = "Error in models";
     if (result.affectedRows) {
         message = "record removed";
